26d19c5373358f57c2c3f420ad44d7c254ffa7d6
Rohan Mathur <rm30092000mr@gmail.com>
2024-06-30 19:31:28 +0530

    Styling of Calendar

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -33,16 +33,20 @@ const Calendar = () => {
     console.log(response?.data);
     if (response?.data) {
       setData(
-        response.data?.map((item: TrackerState) => {
-          let doneThings: Number = Object.values(item).filter(val => val === true)?.length
-          return {
-            ...item,
-            doneThings
-          }
-        })?.sort(
-          (a: TrackerState, b: TrackerState) =>
-            a.day.valueOf() - b.day.valueOf()
-        )
+        response.data
+          ?.map((item: TrackerState) => {
+            let doneThings: Number = Object.values(item).filter(
+              (val) => val === true
+            )?.length;
+            return {
+              ...item,
+              doneThings,
+            };
+          })
+          ?.sort(
+            (a: TrackerState, b: TrackerState) =>
+              a.day.valueOf() - b.day.valueOf()
+          )
       );
     }
   };
@@ -50,30 +54,64 @@ const Calendar = () => {
     getData();
   }, []);
   const renderValue = (item: TrackerState, val: boolean) => {
-    return Object.entries(item).map(([key, value]: [string, boolean], idx: number) => {
-      if (typeof value === "boolean" && value === val) {
-        return <div key={idx}>{mapper[key as keyof TrackerState]}</div>;
+    return Object.entries(item).map(
+      ([key, value]: [string, boolean], idx: number) => {
+        if (typeof value === "boolean" && value === val) {
+          return <div key={idx}>{mapper[key as keyof TrackerState]}</div>;
+        }
+        return null;
       }
-      return null;
-    });
+    );
+  };
+  const renderPercentage = (item: TrackerState) => {
+    const doneThings = item?.doneThings?.valueOf() ?? 0;
+    const percentage = (doneThings / 6) * 100;
+    const formattedPercentage =
+      percentage % 10 !== 0 ? percentage.toFixed(2) : percentage.toFixed(0);
+
+    return `${formattedPercentage}%`;
   };
   return (
     <div className="flex flex-col gap-10 p-2">
-      <div
-        className="px-4 py-2 rounded-md bg-[#609DA1] text-white hover:scale-105 duration-300 ease-in-out cursor-pointer w-fit"
-        onClick={() => navigate("/")}
-      >
-        Create Task
+      <div className="flex justify-between items-center">
+        <div
+          className="px-4 py-2 rounded-md bg-[#609DA1] text-white hover:scale-105 duration-300 ease-in-out cursor-pointer w-fit"
+          onClick={() => navigate("/")}
+        >
+          Add Tasks
+        </div>
+        <div className="mr-4">
+          {data?.filter(it => it?.doneThings === 6)?.length} / 31
+        </div>
       </div>
       <div className="flex gap-5 items-center flex-wrap">
         {data.map((item: TrackerState, index: number) => (
-          <Card key={index} className="flex flex-col gap-2">
-            <p>{item.day.valueOf()}</p>
-            <p>{item.doneThings?.valueOf()}/6</p>
-            <p>Done</p>
-            {renderValue(item, true)}
-            <p>Not Done</p>
-            {renderValue(item, false)}
+          <Card
+            key={index}
+            className={`flex flex-col gap-2 relative h-72 bg-${
+              item.doneThings === 0
+                ? "red"
+                : item.doneThings === 6
+                ? "green"
+                : "yellow"
+            }-200`}
+          >
+            <p className="text-center">{item.day.valueOf()} July, 2024</p>
+            <p className="absolute right-2 top-2 rounded-full bg-white py-2.5 px-1.5">
+              {renderPercentage(item)}
+            </p>
+            {item.doneThings !== 0 && (
+              <>
+                <p className="underline">Done</p>
+                {renderValue(item, true)}
+              </>
+            )}
+            {item.doneThings !== 6 && (
+              <>
+                <p className="underline mt-5">Not Done</p>
+                {renderValue(item, false)}
+              </>
+            )}
           </Card>
         ))}
       </div>
